fcf63ce2d59fdb2f86e51f80637fb9c1f3123ec1
Mario Patronelli <mario@raffles.com.ar>
2021-05-19 10:38:16 -0300

    Add Login controller and routes for api guard. Add RedirectsUsers trait.

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\{ Controller, RedirectsUsers };
+
+use Auth;
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\{ Login, Logout };
+use Illuminate\Validation\ValidationException;
+use RafflesArgentina\RestfulController\Traits\FormatsValidJsonResponses;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use FormatsValidJsonResponses, RedirectsUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api')->only('logout');
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|min:8|string',
+        ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $userModel = config('auth.providers.users.model');
+        $user = (new $userModel)->where($this->username(), $request[$this->username()])->first();
+	
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $this->guard()->setUser($user);
+
+                event(new Login('api', $user, false));
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only($this->username(), 'password');
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $user = $this->guard()->user();
+
+        $token = $user->createToken(env('APP_NAME'));
+        $accessToken = $token->accessToken;
+
+        //$user->loadMissing();
+
+        $token = $user->createToken(env('APP_NAME'));
+        $accessToken = $token->accessToken;
+        $expiresAt = $token->token->expires_at;
+        $tokenType = 'bearer';
+
+        $data = compact('accessToken', 'expiresAt', 'tokenType', 'user');
+
+        return $this->authenticated($request, $user)
+            ?: $this->validSuccessJsonResponse('Login success', $data, $this->redirectPath());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        //
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->token()->revoke();
+
+        event(new Logout('api', $user));
+
+	$this->loggedOut($request);
+
+        return $this->validSuccessJsonResponse('Logout success', [], $this->redirectTo);
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
